2d689c2d20fc4d79996cf9676aa77de4110f2fcb
bqdong <bqdong.dev@gmail.com>
2024-11-01 22:14:37 +0800

    Init

---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,15 @@
+// https://vitepress.dev/guide/custom-theme
+
+// https://github.com/denoland/deno/issues/23319
+// @deno-types="../../global.d.ts"
+import Layout from './Layout.vue'
+import type { Theme } from 'vitepress'
+import './style.css'
+
+export default {
+  Layout,
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  }
+} satisfies Theme
+
