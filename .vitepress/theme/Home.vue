f8ad54bc64cc560d1d01d631f7ccf83ff14e19b4
bqdong <bqdong.dev@gmail.com>
2024-12-02 21:41:21 +0800

    feat: sort post item

--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { data } from "./data-loaders/all-post.data";
+
+const postItems = computed(() => {
+  return data.sort((p1, p2) => p1.postDate > p2.postDate ? -1 : 0);
+});
 </script>
 
 <template>
   <div class="post-items-wrapper">
-    <div v-for="d in data">
+    <div v-for="d in postItems">
       <a :href="d.path">{{ d.title }}</a>
     </div>
   </div>
